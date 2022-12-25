46ec833596b8b9dca5385422e08be4a08099afdd
iamzr <iamziarehman@gmail.com>
2022-12-25 22:07:14 +0000

    Added quick sort and tests

--- algorithms/sorting_algorithms/sorting_algorithms.py
+++ algorithms/sorting_algorithms/sorting_algorithms.py
@@ -2,6 +2,18 @@ from typing import List
 
 
 def merge_sort(data: List[int]) -> List[int] | None:
+    """
+    Merge sort
+
+    Average case: n log(n)
+    Best case: n log(n)
+    Worst case: n log(n)
+    Memory: n
+    Stable: Yes
+
+    @param data: list to be sorted
+    @return: sorted list
+    """
     length_of_data = len(data)
     if length_of_data <= 1:
         return
@@ -30,12 +42,59 @@ def merge_sort(data: List[int]) -> List[int] | None:
             j += 1
         k += 1
 
+    # If you've run out of right elements, add left list elements to the end
     while i < len(left_list):
         data[k] = left_list[i]
         i += 1
         k += 1
 
+    # If you've run out of left elements, add right elements to the end
     while j < len(right_list):
         data[k] = right_list[j]
         j += 1
         k += 1
+
+
+def _partition(list, low, high):
+    # pivot - element to be place into the correct position
+    pivot = list[high]
+
+    # index of smaller element and indicates the correct position of the pivot found so far
+    i = low - 1
+
+    for j in range(low, high):
+        if list[j] <= pivot:
+            i += 1
+            list[i], list[j] = list[j], list[i]
+
+    list[i + 1], list[high] = list[high], list[i + 1]
+
+    return i + 1
+
+
+def _quick_sort_recursive(list, low, high):
+    if low < high:
+        partition = _partition(list, low, high)
+
+        _quick_sort_recursive(list, low, partition - 1)
+        _quick_sort_recursive(list, partition + 1, high)
+
+
+def quick_sort(data: List[int]) -> List[int]:
+    """
+    Quick sort
+
+    Best case: n log(n)
+    Average case: n log(n)
+    Worst case: n^2
+    Memory: log(n)
+    Stable: No
+
+    @param data: list to be sorted
+    @return: sorted list
+    """
+
+    low = 0
+    high = len(data) - 1
+
+    _quick_sort_recursive(data, low, high)
